dac218204eec1d89f426dea54ba0b9bb8cad3bc9
strivitech <strivitech@gmail.com>
2024-03-21 19:09:57 +0200

    Add Blazor server project with basic UI

---
+++ SimpleNotes/Data/NoteModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleNotes.Data;
+
+public class NoteModel
+{
+    public Guid Id { get; set; }
+    
+    [Required]
+    public string Title { get; set; } = null!;
+    
+    [Required]
+    public string Content { get; set; } = null!;
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public bool IsContentVisible { get; set; } = false;
+}
